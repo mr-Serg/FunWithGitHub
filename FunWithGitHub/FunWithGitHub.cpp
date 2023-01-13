eb77a1b8cc6fad81a5000da2ab0d3ce73965422a
LNUitTutor <lnu.it.tutor@gmail.com>
2023-01-13 21:01:37 +0200

    Use add in a loop

--- FunWithGitHub/FunWithGitHub.cpp
+++ FunWithGitHub/FunWithGitHub.cpp
@@ -4,10 +4,19 @@
 
 int main()
 {
-    std::cout << "Input a nuber: ";
+    std::cout << "Input a number: ";
 	double x; std::cin >> x;
 	std::cout << " x + 1 = " << add(x)
 		<< "\n x + 5 = " << add(x, 5.0)
 		<< "\n x - 3 = " << add(x, -3.0) << '\n';
+	double a[] = { 1,2,3,4,5 };
+	const int n = sizeof a / sizeof a[0];
+	double s = 0.0;
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout << ' ' << a[i];
+		s = add(s, a[i]);
+	}
+	std::cout << "\n s = " << s << '\n';
 	system("pause");
 }
