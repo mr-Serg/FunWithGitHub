c2d2595cd6080bfcad7043fc3b656ae9ed636e00
LNUitTutor <lnu.it.tutor@gmail.com>
2023-01-13 20:36:56 +0200

    Update add function

--- FunWithGitHub/FunWithGitHub.cpp
+++ FunWithGitHub/FunWithGitHub.cpp
@@ -7,6 +7,7 @@ int main()
     std::cout << "Input a nuber: ";
 	double x; std::cin >> x;
 	std::cout << " x + 1 = " << add(x)
-		<< "\n x + 5 = " << add(x, 5.0) << '\n';
+		<< "\n x + 5 = " << add(x, 5.0)
+		<< "\n x - 3 = " << add(x, -3.0) << '\n';
 	system("pause");
 }
--- FunWithGitHub/Functions.h
+++ FunWithGitHub/Functions.h
@@ -1,3 +1,4 @@
 #pragma once
 
+// function adds its arguments
 double add(double x, double y = 1.0);
