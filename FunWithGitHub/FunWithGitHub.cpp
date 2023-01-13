6ff0ef46401d326496a5f58982a1cac4814d09d0
Serhiy Yaroshko <lnu.it.tutor@gmail.com>
2023-01-13 20:55:25 +0200

    Add function mult

--- FunWithGitHub/FunWithGitHub.cpp
+++ FunWithGitHub/FunWithGitHub.cpp
@@ -4,10 +4,13 @@
 
 int main()
 {
-    std::cout << "Input a nuber: ";
+    std::cout << "Input a number: ";
 	double x; std::cin >> x;
 	std::cout << " x + 1 = " << add(x)
 		<< "\n x + 5 = " << add(x, 5.0)
 		<< "\n x - 3 = " << add(x, -3.0) << '\n';
+	double p = 1.0; std::cout << " p = " << p;
+	while (p < 1000) std::cout << ' ' << (p = mult(p));
+	std::cout << '\n';
 	system("pause");
 }
--- FunWithGitHub/Functions.h
+++ FunWithGitHub/Functions.h
@@ -2,3 +2,6 @@
 
 // function adds its arguments
 double add(double x, double y = 1.0);
+
+// function multiplies its arguments
+double mult(double x, double y = 2.0);
