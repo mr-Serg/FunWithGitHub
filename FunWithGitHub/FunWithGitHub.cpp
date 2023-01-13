aa00f4635d2f02e30bcf066672e1214abd5e348b
LNUitTutor <lnu.it.tutor@gmail.com>
2023-01-13 21:21:41 +0200

    Update the main

--- FunWithGitHub/FunWithGitHub.cpp
+++ FunWithGitHub/FunWithGitHub.cpp
@@ -9,9 +9,7 @@ int main()
 	std::cout << " x + 1 = " << add(x)
 		<< "\n x + 5 = " << add(x, 5.0)
 		<< "\n x - 3 = " << add(x, -3.0) << '\n';
-	double p = 1.0; std::cout << " p = " << p;
-	while (p < 1000) std::cout << ' ' << (p = mult(p));
-	std::cout << '\n';
+
 	double a[] = { 1,2,3,4,5 };
 	const int n = sizeof a / sizeof a[0];
 	double s = 0.0;
@@ -21,5 +19,9 @@ int main()
 		s = add(s, a[i]);
 	}
 	std::cout << "\n s = " << s << '\n';
+
+	double p = 1.0; std::cout << " p = " << p;
+	while (p < 1000) std::cout << ' ' << (p = mult(p));
+	std::cout << '\n';
 	system("pause");
 }
